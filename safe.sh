a8fe05c873435360f4311f625434983d1063e406
Toni Menzel <toni@okidokiteam.com>
2009-07-10 23:42:18 +0200

    foo

--- safe.sh
+++ safe.sh
@@ -3,3 +3,9 @@ tar cfv /Users/tonit/splinter.git .git
 echo "saving to ipod.."
 cp /Users/tonit/splinter.git "/Volumes/Toni’s iPod/toni/"
 echo "done."
+
+echo "uploading.."
+#scp /Users/tonit/splinter.git tonit@osgify.com:/srv/www/vhosts/osgify.com/httpdocs
+scp -r .git tonit@osgify.com:/srv/www/vhosts/osgify.com/httpdocs/splinter.git
+
+echo "done."
